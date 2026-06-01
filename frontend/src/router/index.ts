f342372115ef47833f22968d70884095b2063180
JC Redmond III <jcredmond1516@gmail.com>
2026-06-01 18:17:50 +0100

    hash routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,8 @@
 import HomePage from '@/views/HomePage.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 export default createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
 
   routes: [
     { path: '/', component: HomePage },
